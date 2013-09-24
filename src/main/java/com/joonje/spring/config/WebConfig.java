7f91d9bb750a4451906eac3e9e0d20f7c7c10d3d
Joonje <Joonje@Adje-pc>
2013-09-25 01:50:55 +0800

    init

---
+++ src/main/java/com/joonje/spring/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.joonje.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry){
+//		
+//	}
+//	@Override
+//	@Bean
+//	public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+//		// Create or let "super" create the adapter
+//		// Then customize one of its properties
+//	}
+}
