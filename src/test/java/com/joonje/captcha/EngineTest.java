67651b7e146353dcc6e74b4b919de180ee0a56a0
Anjunjie <Anjunjie@AnjunjiePC>
2013-12-18 18:23:54 +0800

    jcaptcha

---
+++ src/test/java/com/joonje/captcha/EngineTest.java
@@ -0,0 +1,23 @@
+package com.joonje.captcha;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+import com.octo.captcha.engine.CaptchaEngine;
+import com.octo.captcha.engine.image.gimpy.SimpleListImageCaptchaEngine;
+import com.octo.captcha.service.multitype.GenericManageableCaptchaService;
+
+public class EngineTest {
+
+	@Test
+	public void test() throws Exception {
+		CaptchaEngine captchaEngine = new com.octo.captcha.engine.image.gimpy.DeformedBaffleListGimpyEngine();
+		GenericManageableCaptchaService service = new GenericManageableCaptchaService(captchaEngine, 180, 180000, 75000);
+		BufferedImage image = (BufferedImage) service.getChallengeForID("111");
+		ImageIO.write(image, "jpg", new File("e:/abc.jpg"));
+	}
+}
