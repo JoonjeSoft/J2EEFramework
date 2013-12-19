4ac2613ffce5f11a68269c396dbb49025c8c95ab
Anjunjie <Anjunjie@AnjunjiePC>
2013-12-19 19:39:30 +0800

    jcaptcha 2.0

--- src/test/java/com/joonje/captcha/JCaptcha2Test.java
+++ src/test/java/com/joonje/captcha/JCaptcha2Test.java
@@ -1,6 +1,7 @@
 package com.joonje.captcha;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Date;
 
 import javax.imageio.ImageIO;
 
@@ -11,7 +12,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.octo.captcha.engine.CaptchaEngine;
 import com.octo.captcha.service.CaptchaService;
+import com.octo.captcha.service.captchastore.CaptchaStore;
+import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
+import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "file:C:/Users/Anjunjie/git/J2EEFramework/src/main/webapp/WEB-INF/jcaptcha2_0.xml")
@@ -20,9 +25,18 @@ public class JCaptcha2Test extends AbstractJUnit4SpringContextTests {
 	CaptchaService captchaService;
 	@Test
 	public void test() throws Exception{
-		
 		for(int i=0;i<20;i++){
 			BufferedImage image = (BufferedImage)captchaService.getChallengeForID(String.valueOf(i));
+			ImageIO.write(image, "jpg", new File("e:/abcd"+i+".jpg"));
+		}
+	}
+	@Test
+	public void test1() throws Exception{
+		CaptchaStore captchaStore =  new FastHashMapCaptchaStore();
+		//CaptchaEngine captchaEngine = new HotmailEngine();
+		DefaultManageableImageCaptchaService service = new DefaultManageableImageCaptchaService(captchaStore, null, 180, 100000, 75000);
+		for(int i=0;i<20;i++){
+			BufferedImage image = (BufferedImage)service.getChallengeForID(String.valueOf(i));
 			ImageIO.write(image, "jpg", new File("e:/abcd"+i+".jpg"));
 		}
 	}
