bdb8a18479b3feb725789c7660e6619ab112ea5b
Joonje <Joonje@Adje-pc.mshome.net>
2013-12-23 02:19:34 +0800

    spring start

--- src/main/java/com/joonje/spring/controller/IndexController.java
+++ src/main/java/com/joonje/spring/controller/IndexController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
